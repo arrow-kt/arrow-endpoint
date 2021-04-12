fa895275534fd9c70ed9ba9f8bd99734361ce420
Simon Vergauwen <nomisRev@users.noreply.github.com>
2021-04-12 13:26:10 +0200

    Update examples/build.gradle.kts
    Co-authored-by: Imran Settuba <46971368+i-walker@users.noreply.github.com>

--- examples/build.gradle.kts
+++ examples/build.gradle.kts
@@ -6,7 +6,6 @@ dependencies {
     implementation(project(Libs.core))
     implementation(project(Libs.graphQL))
     implementation(project(Libs.ktor))
-    implementation(Libs.arrowCore)
     implementation(Libs.graphQlServer)
     implementation(Libs.ktorServerCore)
     implementation(Libs.ktorServerNetty)
