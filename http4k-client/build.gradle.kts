db6835363eccf7fac33e01c731c9ad6c7b1e37b3
Simon Vergauwen <nomisRev@users.noreply.github.com>
2021-04-15 10:22:56 +0200

    add comment

--- http4k-client/build.gradle.kts
+++ http4k-client/build.gradle.kts
@@ -3,6 +3,6 @@ dependencies {
   implementation(project(Libs.thoolModel))
   implementation("org.http4k:http4k-core:4.7.0.2")
 
+  // example
   implementation("org.http4k:http4k-client-apache:4.7.0.2")
-
 }
