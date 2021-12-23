0100d46954f69127b3b5ad12b2ab0966b734a46a
clojj <clojj@github.com>
2021-12-23 16:11:52 +0100

    Add Spring Boot application to examples update coroutines, dokka, ktor, spring, undertow, kotest, kotest-gradle

--- examples/build.gradle.kts
+++ examples/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.ir.backend.js.compile
+
 plugins {
   kotlin("jvm")
   application
@@ -11,8 +13,10 @@ dependencies {
   implementation(projects.docs.openapiDocs)
   implementation(libs.ktor.server.core)
   implementation(libs.ktor.server.netty)
+  implementation(libs.spring.boot.starter.webflux)
   implementation(libs.logback.classic)
   implementation(libs.kotlinx.serialization.json)
+  implementation(project(":servers:spring-web-server"))
 }
 
 application {
